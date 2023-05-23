a243afd255be28a4129be71294c5648786cba6c3
Rustamjon Tulanov <r.tulanov01@gmail.com>
2023-05-23 12:14:29 +0500

    add 2_6

--- 2_5.php
+++ 2_5.php
@@ -1,17 +1,37 @@
 <?php
-    class User {
-        private $name;
-        private $age;
+class User {
+    private $name;
+    private $age;
 
-        public function __construct($name, $age) {
-            $this->name = $name;
-            $this->age = $age;
+    public function __construct($name, $age) {
+        $this->name = $name;
+        $this->age = $age;
+    }
+
+    public function set_name($name) {
+        $this->name = $name;
+    }
+    public function get_name() {
+        return $this->name;
+    }
+    // __get MAGIC method
+    public function __get($property) {
+        if(property_exists($this, $property)) {
+            return $this->$property;
         }
     }
 
-    class Customer extends User {
-
+    public function __set($property, $value) {
+        if(property_exists($this, $property)) {
+            $this->$property = $value;
+        }
+        return $this;
     }
-    echo $user1->name;
-    $user1 = new User("John", 40)
+
+}
+
+$user1 = new User("John", 40);
+
+$user1->__set('name', 'Brad');
+echo $user1->__get('name');
 ?>
--- index.php
+++ index.php
@@ -1,12 +1,7 @@
 <?php
     class User {
-        private $name;
-        private $age;
-
-        public function __construct($name, $age) {
-            $this->name = $name;
-            $this->age = $age;
-        }
+        protected $name;
+        protected $age;
 
         public function set_name($name) {
             $this->name = $name;
@@ -14,16 +9,15 @@
         public function get_name() {
             return $this->name;
         }
-        // __get MAGIC method
-        public function __get($property) {
-            if(property_exists($this, $property)) {
-                return $this->$property;
-            }
-        }
-
     }
 
-    $user1 = new User("John", 40);
-    echo $user1->set_name("Brad");
-    echo $user1->get_name();
+    class Customer extends User {
+        public function pay($amount) {
+            return $this->name . " paid $" . $amount;
+        }
+    }
+
+    $c = new Customer;
+    $c->set_name("Brad");
+    echo $c->pay(100);
 ?>
