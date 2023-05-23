7b016e303645f8703323cc6fefd12330f237532f
Rustamjon Tulanov <r.tulanov01@gmail.com>
2023-05-23 10:32:26 +0500

    add user class

--- index.php
+++ index.php
@@ -2,8 +2,21 @@
     class User {
         public $name = "Brad";
 
+        public function say_hello() {
+            return $this->name . " says Hello to everyone";
+        }
+
     }
 
     $user1 = new User;
-    echo $user1->name;
+    echo $user1->name . "<br />";
+    echo $user1->say_hello();
+
+    echo "<br />";
+
+    $user2 = new User;
+    $user2->name = "Traverse";
+    echo $user2->name;
+    echo "<br />";
+    echo $user2->say_hello()
 ?>
